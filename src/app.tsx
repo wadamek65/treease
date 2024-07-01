55f552a8addcc7f6b0a2d8cbd9bfb1ab915fa36d
wadamek65 <wadamek65@gmail.com>
2024-07-01 22:43:03 +0200

    Add a simple navbar

--- src/app.tsx
+++ src/app.tsx
@@ -2,12 +2,14 @@ import { Router } from '@solidjs/router';
 import { FileRoutes } from '@solidjs/start/router';
 import { Suspense } from 'solid-js';
 import './app.css';
+import { Navbar } from '~/components/Navbar';
 
 export default function App() {
 	return (
 		<Router
 			root={(props) => (
 				<>
+					<Navbar />
 					<Suspense>{props.children}</Suspense>
 				</>
 			)}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,27 @@
+import { Component } from 'solid-js';
+import { A } from '@solidjs/router';
+import { GitHub } from '~/components/icons/GitHub';
+
+export const Navbar: Component = () => {
+	return (
+		<nav class="navbar bg-base-100">
+			<div class="flex-1">
+				<A class="btn btn-ghost text-xl" href="/">
+					treease
+				</A>
+			</div>
+			<div class="flex-none">
+				<ul class="menu menu-horizontal items-center px-1">
+					<li>
+						<A href={'https://wadamek.me'}>wadamek.me</A>
+					</li>
+					<li>
+						<A href={'https://github.com/wadamek65/treease'}>
+							<GitHub class="size-8" />
+						</A>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
+};
